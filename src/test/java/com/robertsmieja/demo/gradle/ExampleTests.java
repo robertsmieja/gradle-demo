f117e3fc83c643a7133d75393e89f34acc3d7c76
Robert Smieja <robertsmieja@gmail.com>
2017-03-07 02:55:01 -0500

    Add JUnit and sample Unit Test

---
+++ src/test/java/com/robertsmieja/demo/gradle/ExampleTests.java
@@ -0,0 +1,14 @@
+package com.robertsmieja.demo.gradle;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ExampleTests {
+
+    @Test
+    public void testHelloWorld(){
+        System.out.println("Hello from JUnit!");
+        assertTrue(true);
+    }
+}
